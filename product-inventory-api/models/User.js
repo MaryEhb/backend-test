111904caa69ddc3cfb380744bf12d4a8d19015bd
Mariem Ehab <mariem.ehab234@gmail.com>
2025-01-07 00:07:35 +0200

    CHALLENGE 1: add user model with email, password, and role fields

---
+++ product-inventory-api/models/User.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema({
+    email: { 
+        type: String, 
+        required: [true, "Email is required"], 
+        unique: true,
+        trim: true,
+        lowercase: true,
+        match: [/.+@.+\..+/, 'Please fill a valid email address'],    
+    },
+    password: { 
+        type: String,
+        required: [true, "Password is required"],
+        minlength: [8, "Password must be at least 8 characters"],
+        select: false,
+        validate: {
+            validator: function(v) {
+              return /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/.test(v);
+            },
+            message: 'Password must contain at least one letter, one number, and one special character.'
+        },
+    },
+    role: { 
+        type: String,
+        enum: ["admin", "user"], 
+        default: "user" 
+    }
+}, { timestamps: true });
+
+userSchema.pre("save", async function (next) {
+    try {
+        if (this.isModified("password")) {
+            this.password = await bcrypt.hash(this.password, 12);
+        }
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+userSchema.methods.comparePassword = async function (inputPassword) {
+    return bcrypt.compare(inputPassword, this.password);
+};
+
+export default mongoose.model("User", userSchema);
